f73b46c6a524d02b42990a0391f7b1263c9778ac
Doug <douglas.rice@salesforce.com>
2022-08-05 13:36:43 -0400

    Adding modal logic

--- src/lwc/approveOrRejectTimecardsContainer/approveOrRejectTimecardsContainer.js
+++ src/lwc/approveOrRejectTimecardsContainer/approveOrRejectTimecardsContainer.js
@@ -1,6 +1,7 @@
 import { LightningElement, api } from 'lwc';
 import getRelatedTimecards from '@salesforce/apex/TimecardApprovalController.getRelatedTimecards';
 import createNewTimecard from '@salesforce/apex/TimecardApprovalController.createNewTimecard';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ApproveOrRejectTimecardsContainer extends LightningElement {
     @api recordId;
@@ -33,5 +34,13 @@ export default class ApproveOrRejectTimecardsContainer extends LightningElement 
 
     toggleModal() {
         this.modalShown = !this.modalShown;
+    }
+
+    handleSuccess(event) {
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Success!',
+            message: 'Timecard ' + event.detail.id + ' created successfully!',
+            variant: {label: 'success', value: 'success' },
+        }));
     }
 }
--- src/lwc/approveOrRejectTimecardsTable/approveOrRejectTimecardsTable.js
+++ src/lwc/approveOrRejectTimecardsTable/approveOrRejectTimecardsTable.js
@@ -2,4 +2,14 @@ import { LightningElement, api } from 'lwc';
 
 export default class ApproveOrRejectTimecardsTable extends LightningElement {
     @api timecards;
+
+    columns = [
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'Status', fieldName: 'Status__c' },
+        { label: 'Times Rejected', fieldName: 'Number_of_Times_Rejected__c' }
+    ];
+
+    handleRowSelection(event) {
+        console.log(event);
+    }
 }
